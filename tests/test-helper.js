ac772f38413cea3e010c7233f42ead5461814252
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-01-16 13:21:17 -0500

    feat: Install Ember.

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'talks.moacir.moacir/app';
+import config from 'talks.moacir.moacir/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
