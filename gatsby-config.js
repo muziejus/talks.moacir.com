24e8a0b07db7dc09841bc18d4e417cbcd3ec118a
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2022-01-16 13:12:05 -0500

    Clear out. This is an Ember joint now.

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: `Talks by Moacir P. de Sá Pereira`,
-    siteUrl: `https://talks.moacir.com`,
-  },
-  plugins: [],
-};
-
--- src/components/reveal-js.js
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import Reveal from "reveal.js";
-
-export default function RevealJS({ children }) {
-  return <div className="reveal">{children}</div>;
-}
